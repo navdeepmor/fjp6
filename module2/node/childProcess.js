f6a9bbe32d48947652346d34bc24e70e78f693e0
navdeepmor <navdeepmor.me@gmail.com>
2022-03-09 00:05:36 +0530

    os module

---
+++ module2/node/childProcess.js
@@ -0,0 +1,8 @@
+let cp = require('child_process'); // tells that we would us child process
+//console.log(cp);
+
+/* open calculator with the help of this file */
+cp.execFileSync("calc");  // for linux replace calc with gnome-calculator 
+
+
+
---
+++ module2/node/os.js
@@ -0,0 +1,7 @@
+let os = require('os'); // os system module
+//console.log(os);
+
+console.log(os.hostname());
+console.log(os.arch());
+console.log(os.networkInterfaces());
+console.log(os.userInfo());

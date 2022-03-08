3893f79ddd278231a17733688d89011c5b27388f
navdeepmor <navdeepmor.me@gmail.com>
2022-03-08 21:36:40 +0530

    comments added

--- module2/string.js
+++ module2/string.js
@@ -1,11 +1,11 @@
 let str = "I am string";
 
 /* sliced string */
-let tmp = str.slice(2, 6);
+let tmp = str.slice(2, 6); // extract part of a string
 console.log(tmp);
 
 /* replace method */
-let replacedStr = str.replace("am", "was");
+let replacedStr = str.replace("am", "was"); // replace a part with give word
 console.log(replacedStr);
 
 let t = str.split(" "); // split returns an array

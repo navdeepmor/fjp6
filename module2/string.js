e13e00e0cfaeb69d0dcb72615eb910c8a6b0aa1d
navdeepmor <navdeepmor.me@gmail.com>
2022-03-08 12:39:28 +0530

    Youtube web page clone added

---
+++ module2/string.js
@@ -0,0 +1,12 @@
+let str = "I am string";
+
+/* sliced string */
+let tmp = str.slice(2, 6);
+console.log(tmp);
+
+/* replace method */
+let replacedStr = str.replace("am", "was");
+console.log(replacedStr);
+
+let t = str.split(" "); // split returns an array
+console.log(t);

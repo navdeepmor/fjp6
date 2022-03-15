fecd0ba1ba5fe3a93f1e272ca3ddbe029ff1076d
navdeepmor <navdeepmor.me@gmail.com>
2022-03-15 22:22:51 +0530

    module 2 - fs q2

--- module2/node/questions/q2.js
+++ module2/node/questions/q2.js
@@ -1,26 +1,27 @@
 // given an array arr=[audio,video,image,software,documents,applications,other]
 //make folder for each element in the given array and inside each folder make 4 files of that type
+
 let arr = ["audio", "video", 'image', "software", "documents", "applications", "other"]
 
-let file_extention = {
-    audio: ".mp3",
-    video: ".mp4",
-    image: ".png",
-    software: ".zip",
-    document: ".txt",
-    application: ".exe",
-    other: ""
-}
+let nameArr = ["abc", "xyz", "efg", "def"];
+let extArr = ['.mp3', '.mp4', '.png', '.exe', '.pdf', '.apk', '.rar'];    
 
 let fs = require("fs");
 let path = require("path");
 
-// let pathFolder = path.join(__dirname, "q2Folder", arr[i]);
+let dirPath = path.join(__dirname, "organise");
+if(!fs.existsSync(dirPath)){
+    fs.mkdirSync(dirPath);
+}
 
 for(let i=0;i<arr.length;i++){
-    fs.mkdirSync(arr[i]);
-    let filePath = path.join(__dirname, arr[i], file_extention.arr[i])
-    for(let j=0;j<4;j++){
-        fs.writeFileSync(filePath ,"this is the content inside new file");
+    let folderPath = path.join(dirPath, arr[i]);
+    if(!fs.existsSync(folderPath)){
+        fs.mkdirSync(folderPath);
+    }
+    for(let j=0;j<nameArr.length;j++){
+        let fileName = nameArr[j]+extArr[i];
+        let filePath = path.join(folderPath, fileName);
+        fs.writeFileSync(filePath, "");
     }
 }

f9f434287b96d0fdb800792c56f62aa5ae4466a6
navdeepmor <navdeepmor.me@gmail.com>
2022-03-29 21:22:47 +0530

    folder arranged

--- Dev/module2/node/input.js
+++ Dev/module2/node/input.js
@@ -5,7 +5,7 @@ console.log(cmdlineargs);
 /* in o/p we would get an array that means that process.argv is an array where at 0th & 1st idx it will contain path, at 3rd idx there would be our input */
 /* if we gave a input inside "" then it would be taken as together i.e. mapped to a single idx */
 /* cann't write let - keyword in terminal to take as input */
-/* everything is coming as a string */
+/* everything is coming as a string this is only when we are reading from cmdline otherwise if we write let i = 10 --> here it would be number only */
 
 // let i = cmdlineargs[2]
 // console.log(i);
@@ -17,6 +17,7 @@ let j = parseInt(parseInt(cmdlineargs[2], 10)); /* converts the given string int
 console.log(typeof j)
 j = j + 30;
 console.log(j);
+
 
 
 

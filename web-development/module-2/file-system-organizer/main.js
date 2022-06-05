5c9face16aab08c8fbaca10a339866f8cc986150
Navdeep Mor <navdeepmor.me@gamil.com>
2022-06-05 09:59:28 +0530

    module-2 file-system-orangnizer global implemented

--- web-development/module-2/file-system-organizer/main.js
+++ web-development/module-2/file-system-organizer/main.js
@@ -1,11 +1,12 @@
+#!/usr/bin/env node                                                                                 // 1. #!/usr/bin/env node  [ cmd line or OS would get to know in which ent it need to run this script ]  2. $ npm init -y  [ to link this with npm. With this package.json files would be created ]  3. "bin": { "orgz": "main.js" } [ add it in package.json file ]  4. npm link [ npm link ]
 let inputArr = process.argv.slice(2);                                                               // to fetch the dir path entered by user     
 let fs = require("fs");
 let path = require("path");
-
-// node main.js tree "directoryPath"
-// node main.js organize "directoryPath"
-// node main.js help
-
+                                                /* 
+                                                        node main.js tree "directoryPath"
+                                                        node main.js organize "directoryPath"
+                                                        node main.js help 
+                                                                                                    */
 let types = {
     media: ["mp4", "mkv"],
     archives: ['zip', '7z', 'rar', 'tar', 'gz', 'ar', 'iso', "xz"],
@@ -29,23 +30,52 @@ switch(command) {
 }
 
 function treeFn(dirPath) {
-    console.log("Tree command is implemented for path -> ", dirPath);
+    let destPath;
+    if(dirPath == undefined) {
+        treeHelper(process.cwd(), "");
+        return;
+    } else {
+        let doesExist = fs.existsSync(dirPath);
+        if(doesExist) {
+            treeHelper(dirPath, "");
+        } else {
+            console.log("Please enter a valid path");
+            return;
+        }
+    }
+}
+
+function treeHelper(dirPath, indent) {
+    let isFile = fs.lstatSync(dirPath).isFile();                                                            // to check if the dirPath is file or folder
+    if(isFile){
+        let fileName = path.basename(dirPath);
+        console.log(indent + "├──" + fileName);
+    } else {
+        let dirName = path.basename(dirPath);
+        console.log(indent + "└──" + dirName);
+        let childrens = fs.readdirSync(dirPath);
+        for(let i = 0; i < childrens.length; i++) {
+            let childPath = path.join(dirPath, childrens[i])
+            treeHelper(childPath, indent + "\t");
+        }
+    }
 }
 
 function organizeFn(dirPath) {
-    // console.log("Organize command is implemented for path -> ", dirPath);
-    // 1. input directory path given
+                                                    /* ---- check directory path given ---- */
     let destPath;
     if(dirPath == undefined) {
-        console.log("Please enter the directory path");
+        destPath = path.join(process.cwd(), "organized-files");
+        if(!fs.existsSync(destPath)){
+            fs.mkdirSync(destPath);                                                                         // create organized-files dir
+        }
         return;
     } else {
         let doesExist = fs.existsSync(dirPath);
         if(doesExist){
-            // 2. create organized-files dir
             destPath = path.join(dirPath, "organized-files")
             if(!fs.existsSync(destPath)){
-                fs.mkdirSync(destPath);
+                fs.mkdirSync(destPath);                                                                     // create organized-files dir
             }
         } else {
             console.log("Please enter a valid path");
@@ -56,7 +86,7 @@ function organizeFn(dirPath) {
 }
 
 function organizeHelper(src, dest) {
-    // 3. identify categories of all the files present in the i/p dir
+                                                /* identify categories of all the files present in the i/p dir */
     let childNames = fs.readdirSync(src);
     // console.log(childNames);
     for(let i = 0; i < childNames.length; i++){
@@ -65,7 +95,7 @@ function organizeHelper(src, dest) {
         if(isFile) {
             let category = getCategory(childNames[i]);
             console.log(childNames[i], "belong to -->", category);
-            // 4. copy / cut files to organized-file in repective catogery dir
+                                                /* copy / cut files to organized-file in repective catogery dir */
             sendfile(childAddress, dest, category);
         }
     }
@@ -104,5 +134,5 @@ function helpFn() {
     usage: node main.js [tree <path>]
                         [organize <path>] 
                         [help]
-    `);                                                                                             // In JS, strings are single lined, we can't make multiple line string but with temperate literal - ` ` we can get the string in multiple line & in the same way as we write inside ` `.  
+    `);                                                                                                     // In JS, strings are single lined, we can't make multiple line string but with temperate literal - ` ` we can get the string in multiple line & in the same way as we write inside ` `.  
 }

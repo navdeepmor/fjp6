fb68d2a3e7207607781b7810eb88ab15e9865883
Navdeep Mor <navdeepmor.me@gamil.com>
2022-06-19 21:44:30 +0530

    module-4 activity todo-app

--- web-development/module-4/react/activity/todo-app/src/Components/Todo.js
+++ web-development/module-4/react/activity/todo-app/src/Components/Todo.js
@@ -24,10 +24,12 @@ class Todo extends Component {
     }
 
     handleDelete = (id) => {
+      let narr = this.state.tasks.filter((taskObj) => {
+                  return taskObj.id != id;
+                })
+      // console.log(arr);
       this.setState({
-        tasks: this.state.tasks.filter((taskObj) => {
-          return taskObj.id != id;
-        })
+        tasks: [...narr]
       })
     }
 
@@ -40,10 +42,10 @@ class Todo extends Component {
           <button onClick = { this.handleAddTask }> Add </button>
           <ul>
             {
-              this.state.tasks.map((taskObj, id) => (
+              this.state.tasks.map((taskObj) => (
                 <li key = { taskObj.id }>                                                                                         
                   <p> { taskObj.task } </p>  
-                  <button onClick = { () => this.handleDelete(id) }> Delete </button>                                                                                                                                                                                
+                  <button onClick = { () => this.handleDelete(taskObj.id) }> Delete </button>                                                                                                                                                                                
                 </li>
               ))
             }
@@ -63,4 +65,27 @@ export default Todo;
     2. line 28: use ( ) instide of { } | { } is used to return JSX  
     3. line 46: { this.handleDelete(id) } 
                 above statement is understood by compiler as call this.handleDelete(id) & then fn would be invoked then and there. Here this call can be aviod with giving fn def as () => this.handleDelete(id)
+    4. line 45: if we would have write like below the then at line 75 it wouldn't got this (Problem). 
+                Problem:
+                  this.state.tasks.map(function(taskObj) {
+                    return (
+                      <li key = { taskObj.id }>                                                                                         
+                        <p> { taskObj.task } </p>  
+                        <button onClick = { () => this.handleDelete(taskObj.id) }> Delete </button>                                                                                                                                                                                
+                      </li>
+                    )
+                  }) 
+                Soln:
+                  1. ref line 45
+                  2. this.state.tasks.map(function(taskObj) {
+                      return (
+                        <li key = { taskObj.id }>                                                                                         
+                          <p> { taskObj.task } </p>  
+                          <button onClick = { () => this.handleDelete(taskObj.id) }> Delete </button>                                                                                                                                                                                
+                        </li>
+                      )
+                    }.bind(this)) 
+
+  Issues:
+    1. Add empty string
 */

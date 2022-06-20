2c61f4c997db7795a60a256f45f934ce75a8234d
Navdeep Mor <navdeepmor.me@gamil.com>
2022-06-20 17:07:47 +0530

    Interview execution-context lexical-scope

--- web-development/Interview/execution-context.js
+++ web-development/Interview/execution-context.js
@@ -49,24 +49,23 @@ console.log("line number 1", varName);
 var varName = 10;
 
 function b() {
-    console.log("line number 5", varName);
+    console.log("line number 5", varName);                                                                  // inside b it doesn't have varName thus it will look outside it scope because fn in JS are Lexical Scope i.e. it will check in ec of one in which it was called and incase it doesn't found there then it will look him outside also & so on till gec 
 }
 
 console.log("line number 7", varName);
 
 function fn() {
-    console.log("line number 9", varName);
+    console.log("line number 9", varName);                                                                  // inside fn it have varName & till here it's value is undefined
     var varName = 20;
-    b();
+    b();                                                                                                    // we have call b here, but it's actually def outside therefore in such case we see lexical(literal) scope
     console.log("line number 13", varName);
 }
 fn();
-/* call stack --> 1.gec 2.ec 3.ec */
-/* 1.gec: 1.memory allocate - varName:undefined b:8k fn:11k --> before execution */
-/* 2. ec: 1.memory allocate - varName:undefined */
-/* 3. ec: 1.memory allocate -  */
+/* call stack --> 1.gec 2.ec 3.ec 
+   1.gec: 1.memory allocate - varName:undefined b:8k fn:11k --> before execution
+   2. ec: 1.memory allocate - varName:undefined 
+   3. ec: 1.memory allocate -  
 
-/*
             |---------------------------|                     |                   |
             |  global: {..}  this: {}   |                     |-------------------|
       ec    |                           |                     |                   |
@@ -84,9 +83,19 @@ fn();
              ---------------------------                  11k |  fn() { .. }      |
                      call stack                                -------------------
                                                                       Heap
+
+    Note:
+        this - calc at every gec & ec.
 */
 
-/* ---------------------------------------------------------------- */                                                           
+                                                            /* ------ Scope ------ */
+/*    
+    scope - { }                                                                                                 function fn { .. }  
+
+    Lexical Scope -  immediate outside fn scope                       
+    Scope Chain   -  1. incase a fn doesn't have a variable inside it's scope  2. then it will look in ec of one outside it scope & again if there also it doesn't get that variable  3. then again it will look outside of this in ec of curr parent. 1 - 2 - 3 is know as scope chain   
+*/
+
 
 
 

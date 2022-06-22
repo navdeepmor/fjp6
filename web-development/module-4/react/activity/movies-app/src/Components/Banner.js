4d817ff6540fce7209b99943c506c426f1c86d03
Navdeep Mor <navdeepmor.me@gamil.com>
2022-06-22 12:20:59 +0530

    module-4 movies-app page-1

--- web-development/module-4/react/activity/movies-app/src/Components/Banner.js
+++ web-development/module-4/react/activity/movies-app/src/Components/Banner.js
@@ -1,22 +1,21 @@
-// import { movies } from './getMovies'
+import { movies } from './getMovies'
 import React, { Component } from 'react'
 
 export default class Banner extends Component {
   render() {
-    // let movie = movies.results;
-    let movie = { backdrop_path: "https://img.etimg.com/thumb/msid-90044567,width-650,imgsize-88974,,resizemode-4,quality-100/the-batman.jpgp", original_title: "BATMAN", title: "BATMAN BEGINS", overview: "Batman is a superhero appearing in American comic books published by DC Comics. The character was created by artist Bob Kane and writer Bill Finger, and debuted in the 27th issue of the comic book Detective Comics on March 30, 1939." };
+    let banner = movies.results[0];
     return (
         <>
             {
-                movie === ''?
+                banner === ''?
                 <div className="spinner-border text-secondary" role="status">
                     <span className="visually-hidden">Loading...</span>
                 </div>:
                 <div className="card banner-card" style = { { outline: 'none', border: 'none' } }>
-                    <img src = {`${ movie.backdrop_path }`} className = "card-img-top banner-img" alt = {`${ movie.title }`}/>
+                    <img src = {`https://image.tmdb.org/t/p/original${ banner.backdrop_path }`} className = "card-img-top banner-img" alt = {`${ banner.title }`}/>
                     {/* <div className="card-body"> */}
-                        <h1 className="card-title banner-title">{ movie.original_title }</h1>
-                        <p className="card-text banner-text">{ movie.overview }</p>
+                        <h1 className="card-title banner-title">{ banner.original_title }</h1>
+                        <p className="card-text banner-text">{ banner.overview }</p>
                         {/* <a href="#" className="btn btn-primary">Go somewhere</a> */}
                     {/* </div> */}
                 </div>
--- web-development/module-4/react/activity/movies-app/src/Components/Movies.js
+++ web-development/module-4/react/activity/movies-app/src/Components/Movies.js
@@ -2,36 +2,60 @@ import React, { Component } from 'react'
 import { movies } from './getMovies'
 
 export default class Movies extends Component {
-  render() {
-    let movie = movies.result;
-    return (
-        <>
-            { 
-                movie.length == 0 ? 
-                <div className="spinner-border text-secondary" role="status">
-                    <span className="visually-hidden">Loading...</span>
-                </div>:
-                <div>
-                    <h3 className = 'text-center treanding-heading'> <strong> Trending </strong></h3>
-                    <div className = 'movies-list'>
-                        {
-                            movie.map((movieObj) => (
-                                <div className="card movies-card">
-                                    <img src = {`https://res.cloudinary.com/ybmedia/image/upload/${ movieObj.backdrop_path }`} className = "card-img-top movies-img" alt = {`${ movieObj.title }`} style = { { height: '40vh', width: '20vw' } } />
-                                    {/* <div className="card-body"> */}
-                                        <h1 className="card-title movies-title">{ movieObj.original_title }</h1>
-                                        {/* <p className="card-text movies-text">{ movieObj.overview }</p> */}
-                                        <div className = "button-wrapper">
-                                            <a href="#" className="btn btn-primary">Go somewhere</a>
-                                        </div>
-                                    {/* </div> */}
-                                </div> 
-                            ))
-                        }
+    constructor() {
+        super();
+        this.state = {
+            hover: '',
+            parr: [ 1 ]
+        }
+    }
+
+    render() {
+        let movie = movies.results;
+        return (
+            <>
+                { 
+                    movie.length == 0 ? 
+                    <div className="spinner-border text-secondary" role="status">
+                        <span className="visually-hidden">Loading...</span>
+                    </div>:
+                    <div>
+                        <h3 className = 'text-center treanding-heading'> <strong> Trending </strong></h3>
+                        <div className = 'movies-list'>
+                            {
+                                movie.map((movieObj) => (
+                                    <div className="card movies-card" style = { { border: 'none' } } onMouseEnter = { () => this.setState({ hover: movieObj.id }) } onMouseLeave = { () => { this.setState({ hover: '' }) } } >
+                                        <img src = {`https://image.tmdb.org/t/p/original${ movieObj.backdrop_path }`} className = "card-img-top movies-img" alt = {`${ movieObj.title }`}/>
+                                        {/* <div className="card-body"> */}
+                                            <h5 className="card-title movies-title">{ movieObj.original_title }</h5>
+                                            {/* <p className="card-text movies-text">{ movieObj.overview }</p> */}
+                                            <div className = "button-wrapper movies-button" style = { { display: 'flex', width: '100%', justifyContent: 'center' } }> 
+                                                {
+                                                    this.state.hover == movieObj.id &&
+                                                    <a className="btn btn-primary movies-button"> Add to Favourites </a>
+                                                } 
+                                            </div>
+                                        {/* </div> */}
+                                    </div> 
+                                ))
+                            }
+                        </div>
+                        <div style = { { display: 'flex', justifyContent: 'center' } }>
+                            <nav aria-label="Page navigation example">
+                                <ul className="pagination">
+                                    <li className="page-item"><a className="page-link" href="#">Previous</a></li>
+                                    {
+                                        this.state.parr.map((value) => (
+                                            <li className="page-item"><a className="page-link" href="#"> { value } </a></li>
+                                        ))
+                                    }
+                                    <li className="page-item"><a className="page-link" href="#">Next</a></li>
+                                </ul>
+                            </nav>
+                        </div>
                     </div>
-                </div>
-            }
-        </>
-    )
-  }
+                }
+            </>
+        )
+    }
 }
